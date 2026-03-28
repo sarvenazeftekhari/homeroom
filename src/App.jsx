7af402a384235dc7daca88dee7af65802bc92739
Sarvenaz Eftekhari <sarvenazeftekhari@gmail.com>
2026-03-28 14:44:30 -0400

    first commit - project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className="text-3xl p-8">HomeRoom is alive 🎉</h1>
+    </div>
+  )
+}
+
+export default App
